fc428aa35c67becc32ecb55838e2ac1f056666df
Tanvir <santoahmed225@gmail.com>
2025-01-05 10:35:10 +0600

    practicing variables

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("My First Console");
+﻿using System;
+
+class Test  
+{
+public static void Main (string[] args){
+
+    string productName = "iphone Apple 14";
+    double price = 320.5;
+    string category = "smart phone";
+    bool isAvailable = true;
+    int sold = 5;
+
+    Console.Write ("Name : ");
+    Console.WriteLine (productName);
+    Console.WriteLine (price);
+    Console.WriteLine (category);
+    Console.WriteLine (isAvailable);
+    Console.WriteLine (sold);
+    
+         
+}
+
+
+}
--- ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae3bdbef1699545163e42a8083793a477ebacedb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
